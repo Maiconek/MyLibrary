2c271eb99e41b007b5256cbb9abbb82e8a26282a
Marcin Baranowski <mbaranowski57@gmail.com>
2023-02-07 20:34:46 +0100

    initial commit, project setup

---
+++ src/test/java/pl/edu/ug/mbaranowski/mylibrary/MylibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.ug.mbaranowski.mylibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
